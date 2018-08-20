f8d0257210807cee0ba614d8faba15bcdf71736d
annikaivola <anni.kaivola@gmail.com>
2018-08-20 15:51:17 +0300

    hello world

--- Ideo-APP/App.js
+++ Ideo-APP/App.js
@@ -6,8 +6,6 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello World, this is React Native</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
