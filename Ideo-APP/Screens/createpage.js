90cc3e6a4777186c0ef76a13d8e4600608b2e97c
snvr <sara.snvr@outlook.com>
2018-08-21 13:43:18 +0300

    ladingpag ym

--- Ideo-APP/Screens/createpage.js
+++ Ideo-APP/Screens/createpage.js
@@ -1,4 +1,4 @@
 import React from 'react';
-import Create_input from './create_input';
-import Create_btn from './create_btn';
+import Create_input from './createinput';
+import Create_btn from './createbtn';
 
---
+++ Ideo-APP/Views/createbtn.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+
+export default class Createbtn extends Component {
+    render() {
+      return (
+        <View style={styles.container}>
+  
+  
+          <Text>I D E O</Text>
+  
+  
+        </View>
+      );
+    }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
