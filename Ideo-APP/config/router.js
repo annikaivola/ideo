4c307a001a7dafc3c9483bc2c2953b7c5ee18f97
snvr <sara.snvr@outlook.com>
2018-08-22 10:34:04 +0300

    ei toimi router eikä mikään muukaan 2

--- Ideo-APP/config/router.js
+++ Ideo-APP/config/router.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
-import {StackNavigator, StackActions, NavigationAction} from 'react-navigation'; 
+import {createStackNavigator, navigationOptions} from 'react-navigation'; 
 import Createpage from '../Screens/createpage.js';
 import Loginpage from '../Screens/loginpage.js';
 
-const Nav = StackNavigator({
-    Createpage: { screen :Createpage },
+const Nav = createStackNavigator({
+    Createpage: { screen :Createpage 
+        navigationOptions: 
+    },
     Loginpage: { screen: Loginpage },
   });
 
