cfb09525a90b2e57df5a135915c3c9268bd4439c
annikaivola <anni.kaivola@gmail.com>
2018-08-23 13:30:54 +0300

    kommentti

--- Ideo-APP/Screens/ideafeed.js
+++ Ideo-APP/Screens/ideafeed.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
-import IdeaPostList from '../Views/ideapostlist.js';
-import IdeaBtn from '../Views/ideabtn.js';
-import IdeaInput from '../Views/ideainput.js';
-
-
-export default class IdeaFeed extends Component {
-    render() {
-        return (
-            <View>
-            <Text>IDEO</Text>
-            <IdeaPostList/>
-            <IdeaInput/>
-            <IdeaBtn/>
-            </View>
-        );
-    }
-=======
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
@@ -31,5 +11,4 @@ export default class Ideafeed extends Component {
         );
     }
 
->>>>>>> anni
 }
