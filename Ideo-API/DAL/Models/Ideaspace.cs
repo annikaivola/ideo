52e6e6f34eb92f145ac6542e40777f19624b8420
annikaivola <anni.kaivola@gmail.com>
2018-08-24 14:34:53 +0300

    API juttuja, POST ja GET toimii kaikissa

--- Ideo-API/DAL/Models/Ideaspace.cs
+++ Ideo-API/DAL/Models/Ideaspace.cs
@@ -13,6 +13,7 @@ namespace DAL.Models
         public int IdeaspaceId { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
+        public string PasswordSalt { get; set; }
         public string Description { get; set; }
 
         public ICollection<Idea> Idea { get; set; }
--- Ideo-APP/Views/createbtn.js
+++ Ideo-APP/Views/createbtn.js
@@ -21,4 +21,4 @@ export default class Createbtn extends Component {
       </View>
     );
   }
-
+}
