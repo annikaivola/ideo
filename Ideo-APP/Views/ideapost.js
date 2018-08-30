35afc363a7847074270336e389cba6e1d90c6e46
annikaivola <anni.kaivola@gmail.com>
2018-08-30 18:09:51 +0300

    ideapost nappulat ja navigointi

--- Ideo-APP/Views/ideapost.js
+++ Ideo-APP/Views/ideapost.js
@@ -3,16 +3,23 @@ import { TouchableOpacity, View, Text, Button, StyleSheet, Image } from "react-n
 import { styles } from "../Styles/styles.js";
 import RateBtn from "../Views/ratebtn.js";
 import Ideainput from "./ideainput";
+import {withNavigation} from 'react-navigation';
 
-export default class IdeaPost extends Component {
+class IdeaPost extends Component {
+  static navigationOptions = {
+    header: null
+  };
   constructor(props) {
     super(props);
     this.state = {
-      procon: this.props.procon
+      procon: 1
     }
-
   }
-
+  changeProcon = (vote)=>{
+    this.state.procon = vote;
+    console.log(this.state.procon)
+    this.props.navigation.navigate("Comment", {idea:this.props.item, procon: this.state.procon})
+  }
   render() {
     return (
       <View style={styles.ideapost}>
@@ -20,20 +27,22 @@ export default class IdeaPost extends Component {
           {this.props.idea}
         </Text>
         <View style={styles.ratebtndiv}>
-          <TouchableOpacity onPress={() => this.props.changeProcon(1)}>
-            <Image
-              style={{ height: 50, width: 50, marginRight: '4%' }}
-              source={require("../Assets/images/plus.png")}
-            />
+          <TouchableOpacity onPress={()=>this.changeProcon(1)}>
+          <Image
+            style={{ height: 50, width: 50, marginRight: '4%', marginTop: '4%'}}
+            source={require("../Assets/images/plus.png")}
+          />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.props.changeProcon(-1)}>
-            <Image
-              style={{ height: 50, width: 50 }}
-              source={require("../Assets/images/minus.png")}
-            />
+          <TouchableOpacity onPress={()=>this.changeProcon(-1)}>
+          <Image
+            style={{ height: 50, width: 50, marginTop: '4%' }}
+            source={require("../Assets/images/minus.png")}
+          />
           </TouchableOpacity>
         </View>
+        {/* <RateBtn /> */}
       </View>
     );
   }
 }
+export default withNavigation(IdeaPost);
